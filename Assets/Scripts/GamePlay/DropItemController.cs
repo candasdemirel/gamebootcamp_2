758eb39688752037cfec7c91c6b1d7cb72e8dd3f
cdemirel <candasd@gmail.com>
2020-09-25 14:10:30 +0300

    iinitial commit

---
+++ Assets/Scripts/GamePlay/DropItemController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItemController : MonoBehaviour , IReset
+{
+    [SerializeField] DropItemModel behaviour;
+    private int _damage;
+    private int _score;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        if (behaviour is DamageObject damageObject)
+        {
+            _damage = (behaviour.dropType == PoolObjectType.bomb) ? -damageObject.damage : damageObject.damage;
+        }
+        else if (behaviour is ScoreObject scoreObject)
+        {
+            _score = scoreObject.score;
+        }
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if (behaviour is DamageObject damageObject)
+            {
+                collision.gameObject.GetComponent<PlayerController>().ChangeHitPoint(_damage);
+            }
+            else if (behaviour is ScoreObject scoreObject)
+            {
+                collision.gameObject.GetComponent<PlayerController>().ChangeScore(_score);
+            }
+
+        }
+
+        Reset();
+        ObjectPooler.Instance.PoolDestroy(behaviour.dropType , this.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "SafeArea")
+        {
+            Reset();
+            ObjectPooler.Instance.PoolDestroy(behaviour.dropType, this.gameObject);
+        }
+    }
+
+    public void Reset()
+    {
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.angularVelocity = Vector3.zero;
+    }
+}
+
+[System.Serializable]
+class DropItemModel1
+{
+    public PoolObjectType dropType;
+    public int damage;
+    public int score;
+}
+
---
+++ Assets/Scripts/GamePlay/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    const float _playerSpeed = 10f;
+    [SerializeField] private PlayerModel _playerModel;
+    [SerializeField] private GameObject _healthBar;
+    [SerializeField] private GameObject _scoreGmo;
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    private Rigidbody rb;
+
+    private HeathBarController _heathBarController;
+    private CallBack _dieCallBack;
+
+    public void SetCallBack(CallBack callBack)
+    {
+        _dieCallBack = callBack;
+    }
+
+    private void OnEnable()
+    {
+        Reset();
+        rb = GetComponent<Rigidbody>();
+        _playerModel = new PlayerModel(100, GameManager.Instance.GetMaxScore());
+        _healthBar.SetActive(true);
+        _scoreGmo.SetActive(true);
+        _heathBarController = _healthBar.GetComponent<HeathBarController>();
+        HealthVisualator();
+    }
+
+    private void OnDisable()
+    {
+        _healthBar?.SetActive(false);
+        _scoreGmo.SetActive(false);
+        _dieCallBack = null;
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0, 0);
+        rb.velocity = movement * _playerSpeed;
+
+    }
+
+    public void ChangeHitPoint(int damege)
+    {
+        _playerModel.ChangeHitPoint(damege);
+        if(_playerModel.GetHitPoint() == 0)
+        {
+            Die();
+        }
+        HealthVisualator();
+    }
+
+    public void ChangeScore(int score)
+    {
+        _playerModel.ChangeScore(score);
+        ScoreVisualator();
+    }
+
+    private void ScoreVisualator()
+    {
+        _scoreText.text = _playerModel.GetScore().ToString(); 
+    }
+
+    private void Die()
+    {
+        _dieCallBack();
+        var gameManager = GameManager.Instance;
+        gameManager.SetMaxScore(_playerModel.GetScore());
+        gameManager.SetState(StateType.PreGameState);
+    }
+
+    private void HealthVisualator()
+    {
+        _heathBarController.UpdateSliderValue(_playerModel.GetHitPoint());
+    }
+
+    public void Reset()
+    {
+        gameObject.transform.position = GameValues.Instance.startPosition;
+    }
+}
